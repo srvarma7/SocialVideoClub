6a1beda5c064e3b62923c0fdb85e679def91b57f
srvarma7 <48859937+srvarma7@users.noreply.github.com>
2024-04-16 17:10:38 +0530

    Initial commit

---
+++ SocialVideoClub/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SocialVideoClub
+//
+//  Created by Sai Raghu Varma Kallepalli on 16/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+        
+    }
+
+
+}
+
